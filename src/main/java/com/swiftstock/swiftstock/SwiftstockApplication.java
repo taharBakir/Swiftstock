93caa094760974ed4221470e81a90296381d8f79
Tahar Bakir <tahar.bakir@gmail.com>
2025-04-18 00:54:53 +0200

    Creation du projet

---
+++ src/main/java/com/swiftstock/swiftstock/SwiftstockApplication.java
@@ -0,0 +1,13 @@
+package com.swiftstock.swiftstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiftstock/swiftstock/SwiftstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftstock.swiftstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
